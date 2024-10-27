a73330e03590fae242429c3a778be447eac4e004
sagarkraios <sagar.mv@happiestminds.com>
2024-10-27 13:43:45 +0530

    Merged stash changes

--- src/main/java/com/hm/annotations/Test1.java
+++ src/main/java/com/hm/annotations/Test1.java
@@ -3,5 +3,8 @@ package com.hm.annotations;
 public class Test1 {
 	String a = "abcd";
 	String b = "abcde";
+	public static void main(String[] args) {
+		System.out.println("STash Changes");
+	}
 
 }
