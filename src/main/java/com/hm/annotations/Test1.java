7da807eff0a92fb62b8f386ad95c3ebc1ef0f630
sagarkraios <sagar.mv@happiestminds.com>
2024-10-27 21:02:39 +0530

    Commit2

--- src/main/java/com/hm/annotations/Test1.java
+++ src/main/java/com/hm/annotations/Test1.java
@@ -2,7 +2,8 @@ package com.hm.annotations;
 
 public class Test1 {
 	String a = "abcd";
-	String b = "abcde";
+	String b = "abcdef";
+
 	public static void main(String[] args) {
 		System.out.println("STash Changes");
 	}
---
+++ src/main/java/com/hm/annotations/Test3.java
@@ -0,0 +1,10 @@
+package com.hm.annotations;
+
+public class Test3 {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
